611116ccf5643f1b7fba7a66a87cbd9b7be6fc9b
Blake <itdallasdbw@gmail.com>
2025-06-19 15:42:52 -0500

    2.2 complete. Nothing big

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('dfsc')
+console.log('Hello world')
+console.log('Hello, ' + 'world')
+console.log('David'[0])
+console.log('Hello World'[10])
+console.log(''.length)
+let str='David'
+console.log(str[str.length-1])
+
+// Convert temperature
+// C to F formula
+// F=Cx1.8+32
+// let celsius=10
+// let fahrenheit = conversion
+
+// console.log(result)
+let c=10
+let f=c*1.8+32
+console.log(f)
